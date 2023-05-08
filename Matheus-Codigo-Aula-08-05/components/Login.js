c1c370d54884ea9f2e1bcdeb6ee3ebca853cc660
matt1sx <65613999+matt1sx@users.noreply.github.com>
2023-05-08 19:53:44 -0300

    Add files via upload

---
+++ Matheus-Codigo-Aula-08-05/components/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+import styles from './LoginStyles';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log(`Username: ${username}, Password: ${password}`);
+    // Aqui você pode adicionar sua lógica de autenticação
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Usuário"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button
+        title="Entrar"
+        onPress={handleLogin}
+      />
+    </View>
+  );
+};
+
+export default Login;
---
+++ Matheus-Codigo-Aula-08-05/components/LoginStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginVertical: 10,
+    width: '80%',
+    borderRadius: 5,
+  },
+});
+
+export default styles;
